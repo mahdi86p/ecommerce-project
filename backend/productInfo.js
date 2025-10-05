27cdbc8da2f65a7787a0a82dc417a682ced737d9
mahdi pourhasan <mahdipourhasan8616@gmail.com>
2025-10-05 04:28:08 +0330

    generate html code smaller and better

---
+++ backend/productInfo.js
@@ -0,0 +1,28 @@
+const products = [
+  {
+    productImgSrc: "images/products/athletic-cotton-socks-6-pairs.jpg",
+    productName: "Black and Gray Athletic Cotton Socks - 6 Pairs",
+    productStar: "images/ratings/rating-45.png",
+    productRating: 87,
+    productPrice: "$10.99",
+    productId : 12312,
+  },
+  {
+    productImgSrc: "images/products/athletic-cotton-socks-6-pairs.jpg",
+    productName: "Black and Gray Athletic Cotton Socks - 6 Pairs",
+    productStar: "images/ratings/rating-45.png",
+    productRating: 87,
+    productPrice: "$10.99",
+    productId : 324873298,
+  },
+  {
+    productImgSrc: "images/products/athletic-cotton-socks-6-pairs.jpg",
+    productName: "Black and Gray Athletic Cotton Socks - 6 Pairs",
+    productStar: "images/ratings/rating-45.png",
+    productRating: 87,
+    productPrice: "$10.99",
+    productId : 2374678,
+  },
+];
+
+export default products;
--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,6 +1,7 @@
 import Header from '../../components/Header';
 import CheckmarkIcon from "../../assets/images/icons/checkmark.png"
 import HomeIcon from "../../assets/home-favicon.png"
+import products from "../../../backend/productInfo.js"
 import './HomePage.css'
 
 function HomePage() {
@@ -13,56 +14,59 @@ function HomePage() {
 
             <div className="home-page">
                 <div className="products-grid">
-                    <div className="product-container">
-                        <div className="product-image-container">
-                            <img className="product-image"
-                                src="images/products/athletic-cotton-socks-6-pairs.jpg" />
-                        </div>
 
-                        <div className="product-name limit-text-to-2-lines">
-                            Black and Gray Athletic Cotton Socks - 6 Pairs
-                        </div>
+                    {products.map((product) => {
+                        return (
+                            <div key={product.productId} className="product-container">
+                                <div className="product-image-container">
+                                    <img src={product.productImgSrc} alt="" className="product-image" />
+                                </div>
+                                <div className="product-name limit-text-to-2-lines">
+                                    {product.productName}
+                                </div>
+                                <div className="product-rating-container">
+                                    <img
+                                        className='product-rating-stars'
+                                        src={product.productStar} />
+                                    <div>
+                                        {product.productRating}
+                                    </div>
+                                </div>
 
-                        <div className="product-rating-container">
-                            <img className="product-rating-stars"
-                                src="images/ratings/rating-45.png" />
-                            <div className="product-rating-count link-primary">
-                                87
+                                <div className='product-price'>
+                                    {product.productPrice}
+                                </div>
+
+                                <div className="product-quantity-container">
+                                    <select>
+                                        <option value="1">1</option>
+                                        <option value="2">2</option>
+                                        <option value="3">3</option>
+                                        <option value="4">4</option>
+                                        <option value="5">5</option>
+                                        <option value="6">6</option>
+                                        <option value="7">7</option>
+                                        <option value="8">8</option>
+                                        <option value="9">9</option>
+                                        <option value="10">10</option>
+                                    </select>
+                                </div>
+
+                                <div className="product-spacer"></div>
+
+                                <div className="added-to-cart">
+                                    <img src="images/icons/checkmark.png" />
+                                    Added
+                                </div>
+
+                                <button className="add-to-cart-button button-primary">
+                                    Add to Cart
+                                </button>
                             </div>
-                        </div>
+                        )
+                    })}
 
-                        <div className="product-price">
-                            $10.90
-                        </div>
-
-                        <div className="product-quantity-container">
-                            <select>
-                                <option value="1">1</option>
-                                <option value="2">2</option>
-                                <option value="3">3</option>
-                                <option value="4">4</option>
-                                <option value="5">5</option>
-                                <option value="6">6</option>
-                                <option value="7">7</option>
-                                <option value="8">8</option>
-                                <option value="9">9</option>
-                                <option value="10">10</option>
-                            </select>
-                        </div>
-
-                        <div className="product-spacer"></div>
-
-                        <div className="added-to-cart">
-                            <img src={CheckmarkIcon} />
-                            Added
-                        </div>
-
-                        <button className="add-to-cart-button button-primary">
-                            Add to Cart
-                        </button>
-                    </div>
-
-                    <div className="product-container">
+                    {/* <div className="product-container">
                         <div className="product-image-container">
                             <img className="product-image"
                                 src="images/products/intermediate-composite-basketball.jpg" />
@@ -158,7 +162,7 @@ function HomePage() {
                         <button className="add-to-cart-button button-primary">
                             Add to Cart
                         </button>
-                    </div>
+                    </div> */}
                 </div>
             </div>
         </>
