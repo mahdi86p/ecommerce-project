30f9cc10bb4eb94a53291c261c42bb0a180905fc
mahdi pourhasan <mahdipourhasan8616@gmail.com>
2025-10-07 06:13:48 +0330

    debug not cennect to backend Section

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -8,18 +8,18 @@ import './HomePage.css'
 
 function HomePage() {
     const [products, setProducts] = useState([])
-    const [cart , setCart] = useState([]);
+    const [cart, setCart] = useState([]);
 
     useEffect(() => {
-        axios.get("http://localhost:3000/api/products")
+        axios.get("/api/products")
             .then((response) => {
                 setProducts(response.data)
             })
 
-        axios.get("http://localhost:3000/api/cart-items")
-        .then((response) => {
-            setCart(response.data)
-        })
+        axios.get("/api/cart-items")
+            .then((response) => {
+                setCart(response.data)
+            })
     }, [])
 
     return (
